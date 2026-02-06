ff8dbf0f7a6b6f9425ecf7b275f0a852ab3e30d3
pankaj kumar <168459806+pankaj-kumar069@users.noreply.github.com>
2026-02-06 21:20:12 +0530

    Added fallback route

--- Nursery-seed-management-system/backend/server.js
+++ Nursery-seed-management-system/backend/server.js
@@ -67,6 +67,11 @@ app.use((req, res, next) => {
   res.status(404).json({ message: "Route not found" });
 });
 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // -------------------- GLOBAL ERROR HANDLER --------------------
 /*
 ⚠️ IMPORTANT:
